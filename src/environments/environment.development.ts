f231394c604752ba60ae8afe8a81597dc971f186
AATaimah <129572284+AATaimah@users.noreply.github.com>
2024-10-20 22:27:29 -0400

    completed and connected to firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCb0a91MvEfU0GMvGDfIhYBBu0KXCX8vo8",
+  authDomain: "lab5-d7155.firebaseapp.com",
+  projectId: "lab5-d7155",
+  storageBucket: "lab5-d7155.appspot.com",
+  messagingSenderId: "450097335758",
+  appId: "1:450097335758:web:d8144675187b55a400ef9f"
+};
+
+// Environment configuration
+export const environment = {
+  production: false, // Set to true for production builds
+  firebase: firebaseConfig
+};
+
+
